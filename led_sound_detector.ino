649aadbc30186b6c2847f40df6d6231118576a90
a113ssa <95538451+a113ssa@users.noreply.github.com>
2023-10-23 00:14:05 +0400

    Add LED sound detector code

---
+++ led_sound_detector.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal_I2C.h>
+
+int ledPin = 7;
+int microphoneDigitalPin = 53;
+int digitalState = 0;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  pinMode(ledPin, OUTPUT);
+  pinMode(microphoneDigitalPin, INPUT);
+}
+
+void loop() {
+  microPhoneProcess();
+}
+
+void microPhoneProcess() {
+  digitalState = digitalRead(microphoneDigitalPin);
+
+  if (digitalState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(6000);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
